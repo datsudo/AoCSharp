c0a910256eaa20626ed126b86f05e95a77cc4f04
datsudo <76833632+datsudo@users.noreply.github.com>
2023-12-06 18:11:07 +0800

    d6

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,3 +2,5 @@ global using AoCSharp._01;
 global using AoCSharp._02;
 global using AoCSharp._03;
 global using AoCSharp._04;
+global using AoCSharp._05;
+global using AoCSharp._06;
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// Day1.Part1(); Day1.Part2();
 // Day2.Part1(); Day2.Part2();
 // Day3.Part1(); Day3.Part2();
-Day4.Part1(); Day4.Part2();
+// Day4.Part1(); Day4.Part2();
+// Day5.Part1(); // Day5.Part2();
+Day6.Part1(); Day6.Part2();
